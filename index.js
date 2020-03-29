5f4bc35991ad875f3e4924fe48f342935cfaddbf
Dom Porter <dom@dominicporter.co.uk>
2020-03-29 13:09:55 +0200

    Remove IPs from sample file

--- index.js
+++ index.js
@@ -13,9 +13,8 @@ const searchOpts = {
 const func = () =>{
 client
   // .search('royal navy', APIKEY, searchOpts)
-  .scan('94.236.30.16,  195.130.1.242,  195.130.1.246,  195.130.1.236,  195.130.1.239,  137.116.195.101,  137.116.195.101,  81.145.28.200,  81.145.28.200,  81.145.28.200,  81.145.28.202,  81.145.28.202,  81.145.28.202,  195.130.1.241,  195.130.1.244,  104.125.12.157,  104.74.54.118,  104.71.156.26,  94.236.30.201,  185.13.50.196', APIKEY, searchOpts )
+  .scan('123.456.123.111', APIKEY, searchOpts )
   // .scanResult('DVfxjUgHY6JupYDC', APIKEY, searchOpts )
-  // .scanResult('tzJaLlpFmKCfDcjS', APIKEY, searchOpts )
   .then(res => {
     console.log('Result:');
     console.log(util.inspect(res, { depth: 6 }));

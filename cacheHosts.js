c1159969eacb40f65722ab290fcef2e6700f1abb
Dom Porter <dom@dominicporter.co.uk>
2020-03-29 12:28:51 +0200

    Query list of IPs to cache file

--- cacheHosts.js
+++ cacheHosts.js
@@ -1,22 +1,73 @@
 const util  = require('util');
 const client = require('shodan-client');
+const yargs = require('yargs');
+const fs = require('fs');
 
-const searchOpts = {
-  // facets: 'port:100,country:100',
-  // minify: false,
-};
+// Parameter setup stuff
+const argv = yargs
+    .option('ip-file', {
+        alias: 'i',
+        description: 'Specifies file with IP address (1 per line)',
+        type: 'string',
+    })
+    .option('cache-file', {
+        alias: 'o',
+        description: 'Specifies the JSON file to cache the data in',
+        type: 'string',
+    })
+    .help()
+        .alias('help', 'h')
+        .argv;
+
+const options = {};
+
+// Parse Params & read in source files
+if (argv["ip-file"]) {
+    options.ipFile = argv["ip-file"];
+    console.log(`Using IP list from : ${options.ipFile}`)
+  try{
+    let rawdata = fs.readFileSync(options.ipFile, 'utf8');
+    options.ipList = rawdata.split('\n').filter(a => a!= '');
+    console.log(options.ipList)
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+if (argv["cache-file"]) {
+  options.cacheFile = argv["cache-file"];
+  console.log(`Using Cache file : ${options.cacheFile}`)
+  try{
+    const rawdata = fs.readFileSync(options.cacheFile);
+    console.log(JSON.parse(rawdata))
+    options.hostCache = (JSON.parse(rawdata));
+  } catch {
+    console.log('cache file doesnt exist, will create it.');
+  }
+} else {
+  console.log('No cache file specified, outputting to console');
+}
 
 const APIKEY = 'L4ZzevsLoNr60HJAu4mZYvterlFob19H';
 
+
+
+// Now do the query
 const queryHosts = async (hostList, opts = {}) => {
-  hostList.forEach(async hostIp => {
+  for (hostIp of hostList){
     try{
-    console.log(await client.host(hostIp, APIKEY, opts));
+      result = await client.host(hostIp, APIKEY, opts);
+      // console.log(result);
+      options.hostCache[hostIp] = result;
     } catch(err) {
-      console.log('Error:');
+      console.log(`Error: ${hostIp}`);
       console.log(err);
     }
-  });
+  }
+
+  // console.log(options.hostCache);
+
+  fs.writeFileSync(options.cacheFile, JSON.stringify(options.hostCache, null, 2));
 }
 
-queryHosts(['94.236.30.16',  '195.130.1.242'])
+queryHosts(options.ipList);
